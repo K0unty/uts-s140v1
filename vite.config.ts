c3448bbd76b7bafd1b3b7118c2974b397e3e44a8
K0unty <kountiapi@pm.me>
2024-06-23 10:36:06 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
@@ -10,4 +12,22 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  build: {
+    // Add parserOptions to build
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: ["./tsconfig.json", "./tsconfig.node.json"],
+      tsconfigRootDir: __dirname,
+    },
+  },
+  server: {
+    // Add parserOptions to server
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: ["./tsconfig.json", "./tsconfig.node.json"],
+      tsconfigRootDir: __dirname,
+    },
+  },
 });
